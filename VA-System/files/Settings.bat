ad1ab050ce00b85980799a970e8893b21f01258b
GatorThePlayer <TaegenTibbs123@yahoo.com>
2017-08-31 23:34:54 -0400

    Color File Added

--- VA-System/files/Settings.bat
+++ VA-System/files/Settings.bat
@@ -2,135 +2,61 @@
 :head
 Mode 1000
 
-:colors
-find "0a" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0a) else goto aqua
-goto colorset
-
-:aqua
-find "0b" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0b) else goto red
-goto colorset
-
-:red
-find "0c" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0c) else goto pink
-goto colorset
-
-:pink
-find "0d" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0d) else goto yellow
-goto colorset
-
-:yellow
-find "0e" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0e) else goto white
-goto colorset
-
-:white
-find "0f" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=0f) else goto silver
-goto colorset
-
-:silver
-find "07" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=07) else goto olive
-goto colorset
-
-:olive
-find "06" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=06) else goto purple
-goto colorset
-
-:purple
-find "05" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=05) else goto maroon
-goto colorset
-
-:maroon
-find "04" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=04) else goto teal
-goto colorset
-
-:teal
-find "03" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=03) else goto green
-goto colorset
-
-:green
-find "02" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=02) else goto navy
-goto colorset
-
-:navy
-find "01" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=01) else goto blue
-goto colorset
-
-:blue
-find "09" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=09) else goto gray
-goto colorset
-
-:gray
-find "08" .\VA-System\files\stored\settings\Settings.dll >nul
-if %errorlevel% equ 0 (set colorfile=08) else goto stop
-goto colorset
-
 :colorset
+call .\VA-System\files\apps\color_codes\color.bat
 color %colorfile%
 
-echo -                                                                                                   
-echo                                             -----------.                                            
-echo                                            .dddddddddddh`                                           
-echo                                            +dddddddddddd:                                           
-echo                                            ydddddddddddds                                           
-echo                                           .dddddddddddddd`                                          
-echo                     `-`                   /dddddddddddddd:                   `-`                    
-echo                   `/hdho-`                ydddddddddddddds                `-ohdh/`                  
-echo                 `/hddddddy+-            `-dddddddddddddddd-`            .+yddddddh/`                
-echo               `/ydddddddddddy/.    .-/oyhdddddddddddddddddhhyo/-`    ./yhddddddddddy/`              
-echo              :ydddddddddddddddhs:+yhddddddddddddddddddddddddddddhs/:shdddddddddddddddy:             
-echo              -yddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddy-             
-echo               `oddddddddddddddddddddddddddddhhyyssssyyhhddddddddddddddddddddddddddddo`              
-echo                 :hdddddddddddddddddddddys+:-.```    ```.-:+sydddddddddddddddddddddh:                
-echo                  .sddddddddddddddddhy/-`                    `-/yhdddddddddddddddds.                 
-echo                   `+dddddddddddddy+.        ``........``        .+yddddddddddddd+`                  
-echo                     /ddddddddddh/`      `./oyyhddddddhyyo/.`      `/hdddddddddd:                    
-echo                    `yddddddddd+`     `-oyddddddddddddddddddyo-`     `+ddddddddds`                   
-echo                   `yddddddddh-     `:yddddddddddddddddddddddddy:`     -hddddddddo                   
-echo                   oddddddddy.     .sdddddddddddddddddddddddddddds.     .ydddddddd/                  
-echo                  :ddddddddh.     :dddddddddddddddddddddddddddddddd:     `hdddddddd.                 
-echo         ```.-:/+ohdddddddd-     :dddddddddddddhs++++shddddddddddddd:     -ddddddddyo+/:-.```        
-echo    .ossyhdddddddddddddddds     .ddddddddddddo-        -sdddddddddddd.     sddddddddddddddddhysso.   
-echo    -ddddddddddddddddddddd:     oddddddddddh-            :dddddddddddo     :ddddddddddddddddddddd-   
-echo    -ddddddddddddddddddddd`     hdddddddddd:              +ddddddddddh     `ddddddddddddddddddddd-   
-echo    -ddddddddddddddddddddd     `ddddddddddd`              -ddddddddddd`     ddddddddddddddddddddd-   
-echo    -ddddddddddddddddddddd`     hdddddddddd:              +ddddddddddh     `ddddddddddddddddddddd-   
-echo    -ddddddddddddddddddddd:     oddddddddddh-            :dddddddddddo     :ddddddddddddddddddddd-   
-echo    .+osyyhddddddddddddddds     .ddddddddddddo-        -sdddddddddddd.     sdddddddddddddddhysso+.   
-echo          ``.-:/+ohdddddddd-     :dddddddddddddhs++++shddddddddddddd:     -ddddddddyo+/:-.``         
-echo                  :ddddddddh.     :dddddddddddddddddddddddddddddddd:     `hdddddddd.                 
-echo                   oddddddddy.     .sdddddddddddddddddddddddddddds.     .ydddddddd+                  
-echo                   `yddddddddh-     `:yddddddddddddddddddddddddy:`     -hddddddddo                   
-echo                    `yddddddddd+`     `-oyddddddddddddddddddyo-`     `+ddddddddds`                   
-echo                     /ddddddddddh/`      `./oyyhddddddhyyo/.`      `/hdddddddddd:                    
-echo                   `odddddddddddddy+.        ``........``        .+yddddddddddddd+`                  
-echo                  -yddddddddddddddddhy/-`                    `-/yhddddddddddddddddy-                 
-echo                `/hdddddddddddddddddddddys+:-.```    ```.-:+sydddddddddddddddddddddh/                
-echo               .sddddddddddddddddddddddddddddhhyyssssyyhhdddddddddddddddddddddddddddds.              
-echo              :hddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddh:             
-echo              :ydddddddddddddddhs:+shddddddddddddddddddddddddddddhs/:ohdddddddddddddddy:             
-echo               `/yddddddddddhs/.    .-/oyhdddddddddddddddddhhyo/-`    .:shddddddddddy/`              
-echo                 `/hddddddy/.            `-dddddddddddddddd-`            ./yddddddh/`                
-echo                   `/hdy+-                 yddddddddddddddy                 -+ydh/`                  
-echo                     `-`                   /dddddddddddddd/                   `-`                    
-echo                                           .dddddddddddddd`                                          
-echo                                            ydddddddddddds                                           
-echo                                            +dddddddddddd:                                           
-echo                                            .dddddddddddd`                                           
-echo                                             .----------.                                            
-echo -                                                                                                     
+echo -
+echo                                             -----------.
+echo                                            .dddddddddddh`
+echo                                            +dddddddddddd:
+echo                                            ydddddddddddds
+echo                                           .dddddddddddddd`
+echo                     `-`                   /dddddddddddddd:                   `-`
+echo                   `/hdho-`                ydddddddddddddds                `-ohdh/`
+echo                 `/hddddddy+-            `-dddddddddddddddd-`            .+yddddddh/`
+echo               `/ydddddddddddy/.    .-/oyhdddddddddddddddddhhyo/-`    ./yhddddddddddy/`
+echo              :ydddddddddddddddhs:+yhddddddddddddddddddddddddddddhs/:shdddddddddddddddy:
+echo              -yddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddy-
+echo               `oddddddddddddddddddddddddddddhhyyssssyyhhddddddddddddddddddddddddddddo`
+echo                 :hdddddddddddddddddddddys+:-.```    ```.-:+sydddddddddddddddddddddh:
+echo                  .sddddddddddddddddhy/-`                    `-/yhdddddddddddddddds.
+echo                   `+dddddddddddddy+.        ``........``        .+yddddddddddddd+`
+echo                     /ddddddddddh/`      `./oyyhddddddhyyo/.`      `/hdddddddddd:
+echo                    `yddddddddd+`     `-oyddddddddddddddddddyo-`     `+ddddddddds`
+echo                   `yddddddddh-     `:yddddddddddddddddddddddddy:`     -hddddddddo
+echo                   oddddddddy.     .sdddddddddddddddddddddddddddds.     .ydddddddd/
+echo                  :ddddddddh.     :dddddddddddddddddddddddddddddddd:     `hdddddddd.
+echo         ```.-:/+ohdddddddd-     :dddddddddddddhs++++shddddddddddddd:     -ddddddddyo+/:-.```
+echo    .ossyhdddddddddddddddds     .ddddddddddddo-        -sdddddddddddd.     sddddddddddddddddhysso.
+echo    -ddddddddddddddddddddd:     oddddddddddh-            :dddddddddddo     :ddddddddddddddddddddd-
+echo    -ddddddddddddddddddddd`     hdddddddddd:              +ddddddddddh     `ddddddddddddddddddddd-
+echo    -ddddddddddddddddddddd     `ddddddddddd`              -ddddddddddd`     ddddddddddddddddddddd-
+echo    -ddddddddddddddddddddd`     hdddddddddd:              +ddddddddddh     `ddddddddddddddddddddd-
+echo    -ddddddddddddddddddddd:     oddddddddddh-            :dddddddddddo     :ddddddddddddddddddddd-
+echo    .+osyyhddddddddddddddds     .ddddddddddddo-        -sdddddddddddd.     sdddddddddddddddhysso+.
+echo          ``.-:/+ohdddddddd-     :dddddddddddddhs++++shddddddddddddd:     -ddddddddyo+/:-.``
+echo                  :ddddddddh.     :dddddddddddddddddddddddddddddddd:     `hdddddddd.
+echo                   oddddddddy.     .sdddddddddddddddddddddddddddds.     .ydddddddd+
+echo                   `yddddddddh-     `:yddddddddddddddddddddddddy:`     -hddddddddo
+echo                    `yddddddddd+`     `-oyddddddddddddddddddyo-`     `+ddddddddds`
+echo                     /ddddddddddh/`      `./oyyhddddddhyyo/.`      `/hdddddddddd:
+echo                   `odddddddddddddy+.        ``........``        .+yddddddddddddd+`
+echo                  -yddddddddddddddddhy/-`                    `-/yhddddddddddddddddy-
+echo                `/hdddddddddddddddddddddys+:-.```    ```.-:+sydddddddddddddddddddddh/
+echo               .sddddddddddddddddddddddddddddhhyyssssyyhhdddddddddddddddddddddddddddds.
+echo              :hddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddh:
+echo              :ydddddddddddddddhs:+shddddddddddddddddddddddddddddhs/:ohdddddddddddddddy:
+echo               `/yddddddddddhs/.    .-/oyhdddddddddddddddddhhyo/-`    .:shddddddddddy/`
+echo                 `/hddddddy/.            `-dddddddddddddddd-`            ./yddddddh/`
+echo                   `/hdy+-                 yddddddddddddddy                 -+ydh/`
+echo                     `-`                   /dddddddddddddd/                   `-`
+echo                                           .dddddddddddddd`
+echo                                            ydddddddddddds
+echo                                            +dddddddddddd:
+echo                                            .dddddddddddd`
+echo                                             .----------.
+echo -
 echo -
 echo -
 echo -
@@ -155,7 +81,7 @@ echo --------------------------------------------------
 echo -
 
 :begin
-set /p input=Enter the number of a setting: 
+set /p input=Enter the number of a setting:
 if %input% equ 1 goto color43
 if %input% equ 2 goto update43
 if %input% equ 3 goto login43
@@ -163,7 +89,7 @@ if %input% equ 4 goto reset43
 
 :color43
 echo [You can choose between:] navy, green, teal, maroon, purple, olive, silver, gray, blue, lime, aqua, red, fuchsia, yellow, and white
-set /p color=Please type the name of color: 
+set /p color=Please type the name of color:
 goto lime
 
 :lime
@@ -227,7 +153,7 @@ if %color% equ white echo 0f>.\VA-System\files\stored\settings\Settings.dll
 goto message
 
 :message
-echo [Press enter to save]
+echo [Press Enter to Save]
 
 :stop
 pause >nul
